115bfc2ced0c7b1f3fae6c81e260dcbbd56e9325
Kevin <ktorrescolan@gmail.com>
2022-01-29 00:13:46 -0500

    Add btn CV

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -4,6 +4,7 @@ import small from '../../assets/img/small.jpg'
 import medium from '../../assets/img/medium.jpg'
 import large from '../../assets/img/large.jpg'
 import foto from '../../assets/img/foto.jpg'
+import pdf from '../../assets/cv/cv.pdf'
 
 const Presentation = () => {
   return (
@@ -22,6 +23,11 @@ const Presentation = () => {
         <article className="home-profile">
           <h3>Hola!</h3>
           <p>Soy Frontend developer con 4 meses de experiencia desarrollando y manteniendo aplicaciones web basadas en Vue/Vuex/Vue Router. Estudiante de la carrera de ingeniería de Sistema e Informática en la Universidad Tecnológica del Perú. Me considero una persona autodidacta, apasionado del desarrollo web con JavaScript y dispuesto a afrontar cualquier reto.</p>
+          <div className='home-cv'> 
+            <a href={pdf} download="Curriculum Vitae" >
+              <button>Download CV</button>
+            </a>
+          </div>
         </article>
       </section> 
     </>
